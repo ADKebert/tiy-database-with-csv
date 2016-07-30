347b205360c27f4c92c516b973c58029d752be5a
Alan Kebert <adkebert@yahoo.com>
2016-07-30 17:18:09 -0400

    created report class but still need to test

---
+++ fourth-day.bak/database.rb
@@ -0,0 +1,3 @@
+require_relative 'menu.rb'
+
+Menu.new.interact_with_the_db
---
+++ fourth-day.bak/menu.rb
@@ -0,0 +1,249 @@
+require_relative 'person'
+require_relative 'htmlifier'
+require 'text-table'
+require 'csv'
+
+class Menu
+  BUFFER = "\n" + "-" * 50 + "\n\n"
+  def initialize
+    @people = []
+    read_from_csv
+  end
+
+  def prompt_for_desired_action
+    puts "What would you like to do with the database? Please enter the corresponding letter"
+    puts "Add a person to the database (A)"
+    puts "Search for a person (S)"
+    puts "Delete a person from the database (D)"
+    puts "Create a report (R)"
+    puts "Type (E) when you are finished"
+    gets.chomp.downcase
+  end
+
+  def add_new_person
+    puts "What is the person's name?"
+    name = gets.chomp
+
+    politeness = "Please enter #{name}'s"
+
+    puts "#{politeness} phone number"
+    phone_number = gets.chomp
+
+    puts "#{politeness} address"
+    address = gets.chomp
+
+    puts "#{politeness} position"
+    position = gets.chomp
+
+    puts "#{politeness} salary"
+    salary = gets.chomp
+
+    puts "#{politeness} slack account"
+    slack_account = gets.chomp
+
+    puts "#{politeness} github account"
+    github_account = gets.chomp
+
+    @people << Person.new(name, phone_number, address, position, salary, slack_account, github_account)
+    puts "#{BUFFER}#{@people.last.name} has been added to the database.#{BUFFER}"
+    save_to_csv
+  end
+
+  def get_people_that_match(search_string)
+    @people.select do |person|
+      person.name =~ /#{search_string}/ ||
+      person.slack_account == search_string ||
+      person.github_account == search_string
+    end
+  end
+
+  def describe_a_person(person)
+    puts BUFFER
+    puts "Name: #{person.name}"
+    puts "Phone Number: #{person.phone_number}"
+    puts "Address: #{person.address}"
+    puts "Position: #{person.position}"
+    puts "Salary: #{person.salary}"
+    puts "Slack Account: #{person.slack_account}"
+    puts "Github Account: #{person.github_account}"
+    puts BUFFER
+  end
+
+  def search_prompt
+    puts "Please enter part of the person's name, or their full slack or github account name"
+    name = gets.chomp
+    targets = get_people_that_match(name)
+    if targets.empty?
+      puts "That person was not found"
+      puts BUFFER
+    else
+      targets.each do |target|
+        describe_a_person(target)
+      end
+    end
+  end
+
+  def remove_a_person
+    puts "What is the name of the person you would like to delete from the database?"
+    targets = get_people_that_match(gets.chomp)
+    if targets.empty?
+      puts "That person was not found"
+      puts BUFFER
+    else
+      targets.each do |target|
+        describe_a_person(target)
+        puts "remove this person? (y/n)"
+        response = gets.chomp
+        if response == 'y'
+          @people.delete(target)
+          puts "#{target.name} was removed from the database"
+          puts BUFFER
+          save_to_csv
+        end
+      end
+    end
+  end
+
+  def read_from_csv
+    CSV.foreach('employee.csv', headers: true, header_converters: :symbol) do |person|
+      @people << Person.new(person[:name],
+                            person[:phone],
+                            person[:address],
+                            person[:position],
+                            person[:salary],
+                            person[:slack],
+                            person[:github])
+    end
+  end
+
+  def save_to_csv
+    CSV.open('employee.csv', 'w') do |csv|
+      csv << %w{name phone address position salary slack github}
+      @people.each do |person|
+        csv << [person.name, person.phone_number, person.address, person.position,
+                person.salary, person.slack_account, person.github_account]
+      end
+    end
+  end
+
+  def create_report
+    find_positions.each do |position|
+      employees = get_people_with_job(position)
+      report_table = Text::Table.new
+      report_table.head = ["Salary Min", "Max", "Avg"]
+      report_table.rows << [get_min_salary(employees).round(2),
+                            get_max_salary(employees).round(2),
+                            get_avg_salary(employees).round(2)]
+      report_table.rows << :separator
+      report_table.rows << [{ value: 'Number of employees:', colspan: 2 },
+                            employees.size]
+      names = ""
+      employees.each do |employee|
+        names += "#{employee.name} "
+      end
+      report_table.rows << :separator
+      report_table.rows << ["Names:", { value: names, colspan: 2 }]
+      puts report_table
+    end
+  end
+
+  def create_html_report
+    File.open("report.html", "w") do |report_file|
+      report = Htmlifier.new
+      report.title = "Salary Report"
+      report.add_line(report.h1("Salary Report"))
+      find_positions.each do |position|
+        employees = get_people_with_job(position)
+        report.add_line(report.div_start("report-block"))
+        report.add_line(report.h2("Position: #{position}"))
+        report.add_line(report.ul_start)
+        report.add_line(report.li(report.p("Salary Min: #{get_min_salary(employees).round(2)}")))
+        report.add_line(report.li(report.p("Max: #{get_max_salary(employees).round(2)}")))
+        report.add_line(report.li(report.p("Average: #{get_avg_salary(employees).round(2)}")))
+        report.add_line(report.ul_end)
+        report.add_line(report.p("Number of employees: #{employees.size}"))
+        report.add_line(report.p("Names:"))
+        report.add_line(report.ul_start)
+        employees.each do |employee|
+          report.add_line(report.li(report.p(employee.name)))
+        end
+        report.add_line(report.ul_end)
+      end
+      report_file.puts report
+    end
+  end
+
+  def prompt_for_kind_of_report
+    puts "Would you like to print the report to terminal (T)"
+    puts "or save to html? (H)"
+    response = gets.chomp.downcase
+  end
+
+  # Returns a list of unique postions in the database
+  def find_positions
+    positions = []
+    @people.each do |person|
+      unless positions.include? person.position
+        positions << person.position
+      end
+    end
+    positions
+  end
+
+  def get_min_salary(employees)
+    min = Float::INFINITY
+    employees.each do |employee|
+      if employee.salary.to_f < min
+        min = employee.salary.to_f
+      end
+    end
+    min
+  end
+
+  def get_max_salary(employees)
+    max = 0
+    employees.each do |employee|
+      if employee.salary.to_f > max
+        max = employee.salary.to_f
+      end
+    end
+    max
+  end
+
+  def get_avg_salary(employees)
+    sum = 0
+    employees.each do |employee|
+      sum += employee.salary.to_f
+    end
+    sum / employees.length
+  end
+
+  def get_people_with_job(job)
+    @people.select do |person|
+      person.position == job
+    end
+  end
+
+  def interact_with_the_db
+    done = false
+    until done
+      case prompt_for_desired_action
+      when 'a'
+        add_new_person
+      when 's'
+        search_prompt
+      when 'd'
+        remove_a_person
+      when 'r'
+        if prompt_for_kind_of_report == 't'
+          create_report
+        else
+          create_html_report
+          puts "#{BUFFER}saved as report.html#{BUFFER}"
+        end
+      else
+        done = true
+      end
+    end
+  end
+end
